feba17fe8d495c2b1e38e8ead9656b8e01f6f1af
yasmin-14 <yasminftmhrsyh027@gmail.com>
2025-12-05 09:38:52 +0700

    fix normalize

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const cors = require('cors');
 const db = require('./db');
 const jwt = require('jsonwebtoken'); 
-const { protect } = require('./middleware/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3300;
